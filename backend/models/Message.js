5a4ce8b8035f0711605872f557c874807ca9346d
Anup Kumar <97333000+anup2702@users.noreply.github.com>
2025-08-14 21:25:48 +0530

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
   room: { type: String, required: true },
@@ -7,4 +7,4 @@ const messageSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now, index: { expires: "24h" } }
 });
 
-module.exports = mongoose.model("Message", messageSchema);
+export default mongoose.model("Message", messageSchema);
